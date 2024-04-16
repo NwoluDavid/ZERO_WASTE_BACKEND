8463abff18d6be09d7ed86af87b9bd4263e87124
NwoluDavid <nwoludave@gmail.com>
2024-04-16 12:25:41 +0100

    update: added status code and json response to user registration route

--- app/models.py
+++ app/models.py
@@ -9,10 +9,10 @@ from sqlalchemy.orm import registry
 
 
 class UserCreate(SQLModel):
-    display_name:str = Field(min_length=3, max_length=50, description="Name of the User", schema_extra={'example': "A very nice Item"}, title="Name")  # noqa
+    display_name:str = Field(min_length=3, max_length=50, description="Name of the User", schema_extra={'example': "John Doe"}, title="Name" , examples ="Nwolu David")  # noqa
     email: EmailStr = Field(sa_column=Column("email", VARCHAR, unique=True, index=True), description="Email of the passenger",)
-    phone_number: PhoneNumber = Field(description="Phone number of the passenger", title="Phone Number")  # noqa
-    password: str = Field(min_length=8, max_length=100, description="Password of the passenger",title="Password")  # noqa        
+    phone_number: PhoneNumber = Field(description="Phone number of the user", title="Phone Number" ,examples = "+2348103896233")  # noqa
+    password: str = Field(min_length=8, max_length=100, description="Password of the user",title="Password" , examples ="Dante@123")  # noqa        
     class Config:
         orm_mode = True
 class User(UserCreate, table=True):
@@ -20,8 +20,8 @@ class User(UserCreate, table=True):
     waste: list["Waste"] = Relationship(back_populates="user")
     
 class UserLogin(SQLModel):
-    email: EmailStr = Field(description="Email of the passenger",)
-    password: str = Field(min_length=8, max_length=100, description="Password of the passenger",title="Password")  # noqa
+    email: EmailStr = Field(description="Email of the user",)
+    password: str = Field(min_length=8, max_length=100, description="Password of the user",title="Password")  # noqa
     
 class Token(SQLModel):
     access_token: str
@@ -60,6 +60,8 @@ class Booking (SQLModel):
     class Config:
         orm_mode = True
 
+
+# This model is for booking wastes disposal
 class Waste(Booking,table=True):
     id:  Optional[int] = Field(default=None, primary_key=True)
     user_id: int | None = Field(default=None, foreign_key="user.id")
