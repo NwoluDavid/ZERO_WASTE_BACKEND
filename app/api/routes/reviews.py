6ec12773b178d4367a2e796f1ac9ad142053bd02
NwoluDavid <nwoludave@gmail.com>
2024-04-16 22:43:28 +0100

    update : Added Alembic functionalities

--- app/api/routes/reviews.py
+++ app/api/routes/reviews.py
@@ -8,24 +8,24 @@ from app.models import Review
 router = APIRouter()
 
 
-@router.post("/reviews/", response_model=Review)
+@router.post("/reviews/", response_model=Review , status_code =201)
 def create_review_route(reviewer_name: str, rating: int, comment: str, user_id: int, db: Session = Depends(get_db)):
     review = create_review(db, reviewer_name, rating, comment, user_id)
     return review
 
-@router.put("/reviews/{review_id}/", response_model=Review)
+@router.put("/reviews/{review_id}/", response_model=Review,status_code =201)
 def update_review_route(review_id: int, rating: int, comment: str, db: Session = Depends(get_db)):
     review = update_review(db, review_id, rating, comment)
     if not review:
         raise HTTPException(status_code=404, detail="Review not found")
     return review
 
-@router.get("/reviews/user/{user_id}/", response_model=List[Review])
+@router.get("/reviews/user/{user_id}/", response_model=List[Review] ,status_code =200)
 def get_reviews_by_user_id_route(user_id: int, db: Session = Depends(get_db)):
     reviews = get_reviews_by_user_id(db, user_id)
     return reviews
 
-@router.delete("/reviews/{review_id}/", response_model=Review)
+@router.delete("/reviews/{review_id}/", response_model=Review , status_code =200)
 def delete_review_route(review_id: int, db: Session = Depends(get_db)):
     review = delete_review(db, review_id)
     if not review:
--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from sqlmodel import create_engine, Session, SQLModel
 import json
 # from app.config import DATABASE_URL
 
-sqlite_file_name = "database.db"
+sqlite_file_name = "database.sqlite"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url)
