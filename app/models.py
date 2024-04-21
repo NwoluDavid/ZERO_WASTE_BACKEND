9d0bc22f2df0e304a207a49729c8f767f4d92c0f
NwoluDavid <nwoludave@gmail.com>
2024-04-21 05:27:18 +0100

    update Review route & Booking Waste routes

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Field, Column, VARCHAR ,Relationship ,Enum
 from pydantic import EmailStr 
 from pydantic_extra_types.phone_numbers import PhoneNumber
 from typing import Optional , List
-from datetime import date
+from datetime import date  
 from enum import Enum
 from sqlalchemy.orm import registry
 import uuid
@@ -10,9 +10,9 @@ import uuid
 
 
 class UserCreate(SQLModel):
-    display_name:str = Field(min_length=3, max_length=50, description="Name of the User", schema_extra={'example': "John Doe"}, title="Name")  # noqa
+    display_name:str = Field(min_length=3, max_length=50, description="Name of the User",  schema_extra={"examples": ["Jonah"]}, title="Name")  # noqa
     email: EmailStr = Field(sa_column=Column("email", VARCHAR, unique=True, index=True), description="Email of the user",schema_extra={'example': "dave@example.com"})
-    phone_number: PhoneNumber = Field(description="Phone number of the user", title="Phone Number" ,schema_extra={'example': "+2348103896344"})  # noqa
+    phone_number: PhoneNumber = Field(description="Phone number of the user", title="Phone Number" ,schema_extra ={"examples":["+2348103896322"]})  # noqa
     password: str = Field(min_length=8, max_length=100, description="Password of the user",title="Password" , schema_extra={'example': "Dante@123"})  # noqa       
 
     class Config:
@@ -52,10 +52,10 @@ class UserOutput(SQLModel):
     phone: PhoneNumber
     
 class WasteType(str, Enum):
-    organic_waste = "Organic waste"
-    plastic_waste ="Industrial waste"
-    medical_waste = "Medical waste"
-    industrial_waste = "Industrial waste"
+    organic_waste = "Organic"
+    plastic_waste ="Plastic"
+    medical_waste= "Medical"
+    industrial_waste = "Industrial"
     
     
 class Amount(int, Enum):
@@ -67,15 +67,16 @@ class Amount(int, Enum):
 class BookingStatus(str, Enum):
     pending ="PENDING"
     in_transit ="IN TRANSIT"
-    delivered ="DELIVERED"   
+    delivered ="COMPLETED"   
+ 
     
 class Booking (SQLModel):
-    first_name:str
-    last_name: str
-    phone:PhoneNumber
-    address: str
-    pickup_date: date
-    waste_type:WasteType 
+    first_name:str = Field(description="user firstname" ,  schema_extra={"examples": ["Jonah"]})
+    last_name: str =Field(description="user last name" , schema_extra={"examples": ["Onah"]})
+    phone:PhoneNumber =Field(description="the user phonenumber" , schema_extra ={"examples":["+2348103896322"]})
+    address: str =Field(description ="User address for pickup",schema_extra={"examples": ["Predia Hotel"]})
+    pickup_date:Optional[date]
+    waste_type:WasteType = Field(description ="Input the type of waste",schema_extra={"examples": ["Plastic"]})
     user_waste: Optional[str]=None
    
     class Config:
@@ -85,10 +86,10 @@ class Booking (SQLModel):
 # This model is for booking wastes disposal
 class Waste(Booking,table=True):
     id:  Optional[int] = Field(default=None, primary_key=True, index =True)
-    user_id: uuid.UUID | None = Field(default_factory=uuid.uuid4, foreign_key="user.id")
+    user_id: uuid.UUID | None = Field(default=uuid.uuid4, foreign_key="user.id")
     user: User | None = Relationship(back_populates="waste")
-    amount:Optional[Amount]
-    order_status:Optional[BookingStatus]
+    amount:Optional[Amount] =Field(default = 1000)
+    order_status:Optional[BookingStatus] = Field(default = "PENDING")
     
 
 class UserUpdate(SQLModel):
@@ -108,7 +109,7 @@ class ReviewBase(SQLModel):
 
 class Review(ReviewBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True, index=True)
-    user_id: uuid.UUID|None = Field(default=None, foreign_key="user.id")
+    user_id: uuid.UUID | None = Field(default_factory=uuid.uuid4, foreign_key="user.id")
     user: User | None = Relationship(back_populates="review")
     
 
