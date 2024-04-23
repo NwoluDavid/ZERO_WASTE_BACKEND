5a849b49af0fe310c910a79258420dfc8f8ecfc5
NwoluDavid <nwoludave@gmail.com>
2024-04-23 01:03:13 +0100

    code refactor

--- app/api/routes/test_token.py
+++ app/api/routes/test_token.py
@@ -5,9 +5,9 @@ from app.models import User, UserOutput
 from typing import Annotated, Any
 
 
-
-
 router =APIRouter()
+
+
 @router.post("/login/test-token", response_model=UserOutput)
 def test_token(current_user: Annotated[User, Depends(get_current_user)]
 ) -> Any:
--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,19 +1,18 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Query, Header, Request
-from sqlmodel import Session, select
+from fastapi import APIRouter, Depends, HTTPException, status, Request
+from sqlmodel import Session
+
 from app.deps import get_db, get_current_user
-from app.models import UserCreate, User, UserLogin, Token, UserOutput,UserUpdate
+from app.models import UserCreate, User, UserUpdate
+
 from typing import Annotated, Any
-from fastapi.security import OAuth2PasswordRequestForm
-from app.utils import get_password_hash, verify_password, create_token
-from datetime import timedelta
-from app.config import settings
-from app.crud import update_user, get_user_by_id, delete_user
+from app.utils import get_password_hash
+
 from sqlalchemy.orm import registry
 from sqlalchemy.exc import IntegrityError
+
 from fastapi.responses import JSONResponse
-from app.schemas import SignUpModel
 from fastapi.encoders import jsonable_encoder
-import uuid
+
 
 
 
@@ -48,15 +47,11 @@ async def register(
         user.password = get_password_hash(user.password)
         
         new_user =User(**user.model_dump())
-        # new_user = User( 
-        #     display_name= user.display_name,       
-        #     email=user.email,
-        #     email_verified=False,
-        #     phone_number=user.phone_number.split(":")[1],
-        #     password=user.password)
         db.add(new_user)
         db.commit()
         db.refresh(new_user)
+        
+        
     except IntegrityError as e:
         error_message ="Email already exists"
         raise HTTPException(status_code=409, detail=error_message)
