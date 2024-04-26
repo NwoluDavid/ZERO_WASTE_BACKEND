7fecf62e532a0994f6cfa268a21abadcf08a0c19
NwoluDavid <nwoludave@gmail.com>
2024-04-26 12:16:58 +0100

    updated email verification route

--- app/api/routes/login.py
+++ app/api/routes/login.py
@@ -59,6 +59,10 @@ async def login_verify_email(email:str , db: Annotated[Session, Depends(get_db)]
     """"This route allows the user to still verify his email , when logged in.
     note a user is still allowed to login when he/she registers.
     """
+    if user is None:
+            return  HTTPException(status_code = status.HTTP_400_BAD_REQUEST, 
+            detail="User is not registered"
+        )
         
     if user.is_active:
         return  HTTPException(status_code = status.HTTP_400_BAD_REQUEST, 
