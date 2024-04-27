ee10104f2e8bd4b1cdbcd983845ace57c2417220
NwoluDavid <nwoludave@gmail.com>
2024-04-27 04:54:32 +0100

    updated payment

--- app/api/routes/payment.py
+++ app/api/routes/payment.py
@@ -4,7 +4,7 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from app.utils import verify_payment
 
-from app.models import Waste
+from app.models import Waste , BookingStatus
 from typing import Annotated
 
 from sqlmodel import Session
@@ -18,8 +18,15 @@ async def verify_transaction(ref_id: str,  db: Annotated[Session, Depends(get_db
     payment_status,data = verify_payment(ref_id)
     if payment_status == True:
     # booking = db.query(Waste).find(Waste.id == ref_id)
-        booking = db.get(Waste, ref_id)
-        booking.order_status = "COMPLETED"
+        booking = db.get(Waste, int(ref_id))
+        booking.order_status = BookingStatus.delivered
+        
+        
+    for field, value in booking.dict().items():
+        setattr(booking, field, value)
+    
+    db.commit()
+    db.refresh(booking)
     
     booking = jsonable_encoder(booking)
     
